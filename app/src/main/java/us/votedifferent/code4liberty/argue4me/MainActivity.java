b0d6640a6d8fde66ef1b1919f3bafc6deb96d6c6
Scottt <scott.geyer@eagles.usm.edu>
2016-07-01 21:00:49 -0500

    fixed array index & trying to iterate through war
    set array index to 0 on buttons with only 1 argument. Trying to iterate
    through war with global int variable.

--- app/src/main/java/us/votedifferent/code4liberty/argue4me/MainActivity.java
+++ app/src/main/java/us/votedifferent/code4liberty/argue4me/MainActivity.java
@@ -1,12 +1,29 @@
 package us.votedifferent.code4liberty.argue4me;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.util.Random;
+
+
 public class MainActivity extends AppCompatActivity {
+
+    // iteration variable for foreign policy
+    public int wi = 0;
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +36,16 @@ public class MainActivity extends AppCompatActivity {
         Button warButton = (Button) findViewById(R.id.war);
         Button corpButton = (Button) findViewById(R.id.corp);
 
+        final String[] drugList = {"Who owns your body? You or the government?"};
+
+        final String[] taxList = {"Taxation is theft!"};
+
+        final String[] econList = {"End all corporate welfare."};
+
+        final String[] warList = {"Free trade with all. Entangling alliances with no one.", "War is the health of the state"};
+        final int warSize = warList.length;
+
+
         final String drugLine = "Who owns your body? You or the government?";
         final String taxLine = "Taxation is theft!";
         final String corpLine = "End all corporate welfare.";
@@ -27,36 +54,83 @@ public class MainActivity extends AppCompatActivity {
         drugButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendLine(drugLine);
+
+                Random rand = new Random();
+                int w = + rand.nextInt((warSize) + 1);
+                sendLine(taxList[w]);
+
             }
         });
 
         taxButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendLine(taxLine);
+                sendLine(taxList[0]);
             }
         });
 
         corpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendLine(corpLine);
+                sendLine(econList[0]);
             }
         });
 
         warButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sendLine(warLine);
+                sendLine(warList[0]);
             }
         });
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     private void sendLine(String argument) {
         Intent intent = new Intent(this, argumentActivity.class);
         intent.putExtra("Argument", argument);
         startActivity(intent);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://us.votedifferent.code4liberty.argue4me/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://us.votedifferent.code4liberty.argue4me/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
     }
 }
