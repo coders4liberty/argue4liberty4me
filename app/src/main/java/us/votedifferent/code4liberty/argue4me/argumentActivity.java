04db7ed22f697bee944df0e2e0cede1c1cc29e3f
Scottt <scott.geyer@eagles.usm.edu>
2016-07-02 00:00:57 -0500

    added a copy to clipboard button

--- app/src/main/java/us/votedifferent/code4liberty/argue4me/argumentActivity.java
+++ app/src/main/java/us/votedifferent/code4liberty/argue4me/argumentActivity.java
@@ -1,34 +1,105 @@
 package us.votedifferent.code4liberty.argue4me;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
 public class argumentActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_argument);
 
-        Bundle pickupData = getIntent().getExtras();
-        Button retryButton = (Button) findViewById(R.id.tryAgain);
+        final Bundle pickupData = getIntent().getExtras();
+        Button copyButton = (Button) findViewById(R.id.copy);
 
         if (pickupData == null) {
             return;
         }
-            String receivedArgument = pickupData.getString("Argument");
-            TextView newLine = (TextView) findViewById(R.id.Argument);
-            newLine.setText(receivedArgument);
+        final String receivedArgument = pickupData.getString("Argument");
+        TextView newLine = (TextView) findViewById(R.id.Argument);
+        newLine.setText(receivedArgument);
 
-        retryButton.setOnClickListener(new View.OnClickListener() {
+        copyButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
-            }
-            });
+                // if the user selects copy
+                //case R.id.menu_copy:
 
-        }
+// Gets a handle to the clipboard service.
+                ClipboardManager clipboard = (ClipboardManager)
+                        getSystemService(Context.CLIPBOARD_SERVICE);
+// Creates a clip object with the Intent in it. Its label is "Intent" and its data is
+// the Intent object created previously
+                ClipData clip = ClipData.newPlainText("argument", receivedArgument);
+
+                // Set the clipboard's primary clip.
+                clipboard.setPrimaryClip(clip);
+                TextView newLine = (TextView) findViewById(R.id.copy);
+                String copied = "COPIED TO CLIPBOARD";
+                newLine.setText(copied);
+            }
+        });
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "argument Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://us.votedifferent.code4liberty.argue4me/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "argument Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://us.votedifferent.code4liberty.argue4me/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
+}
